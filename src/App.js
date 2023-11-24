d4f65da2b4907f1d04c7a4620fd1473f541d31ea
AakritiMeh <118741534+AakritiMeh@users.noreply.github.com>
2023-11-24 21:43:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./Navbar";
 import "./App.css";
-import Home from "./components/Home";
+import Home from "./Home";
 function App() {
   return (
     <>
